bf1bb600e431e590fabc7ed7906cd0dea1461f77
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 <erushblu2@gmail.com>
2026-07-18 21:41:33 -0300

    adicionado comando eval

---
+++ src/commands/dev/eval.js
@@ -0,0 +1,33 @@
+import { inspect } from "node:util";
+import { command } from "../../factories/command.js";
+
+export default command({
+  name: "eval",
+  aliases: ["ev", "e"],
+  devOnly: true,
+
+  async run(client, message, args) {
+    if (!args.length) {
+      return message.reply("Forneça um código para avaliar.");
+    }
+
+    const code = args.join(" ");
+
+    try {
+      const result = await eval(code);
+
+      const output = inspect(result, {
+        depth: 3,
+        colors: false,
+      })
+        .replaceAll("```", "\\`\\`\\`")
+        .slice(0, 1990);
+
+      await message.reply(`\`\`\`js\n${output}\n\`\`\``);
+    } catch (error) {
+      const output = `${error.name}: ${error.message}`;
+
+      await message.reply(`\`\`\`js\n${output}\n\`\`\``);
+    }
+  },
+});
--- src/structures/ExtendedClient.js
+++ src/structures/ExtendedClient.js
@@ -2,13 +2,21 @@ import { Client, Collection } from "discord.js";
 import { loadCommands } from "../loaders/loadCommands.js";
 import { loadEvents } from "../loaders/loadEvents.js";
 import { loadInteractions } from "../loaders/loadInteractions.js";
+import { TwinDB } from "twin-db";
 
 export class ExtendedClient extends Client {
-  constructor({ intents = 3276799, prefix = "c,", developers = [] }) {
+  constructor({
+    intents = 3276799,
+    prefix = "c,",
+    developers = ["1208958778243031060"],
+  }) {
     super({ intents });
 
     this.commands = new Collection();
     this.components = new Collection();
+    this.database = {
+      users: new TwinDB("database/users"),
+    };
 
     this.config = {
       prefix,
